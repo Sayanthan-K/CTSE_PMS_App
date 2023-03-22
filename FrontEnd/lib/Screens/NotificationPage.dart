3dcbfadbcf67fb80f3a289e93c72c0f007ca09ed
Sayanthan-K <kathirsayanthan@gmail.com>
2023-03-23 04:37:20 +0530

    finished the notification page

--- FrontEnd/lib/Screens/NotificationPage.dart
+++ FrontEnd/lib/Screens/NotificationPage.dart
@@ -41,118 +41,118 @@ class _NotificationPageState extends State<NotificationPage> {
     ];
     return Scaffold(
       appBar: CustomAppBar(context, "Notification"),
-      // body: SingleChildScrollView(
-      //   child: Container(
-      //     margin: EdgeInsets.all(10),
-      //     padding: EdgeInsets.all(10),
-      //     decoration: BoxDecoration(
-      //       // color: Colors.lightGreen[400],
-      //       color: Color.fromARGB(255, 255, 255, 255),
-      //       border: Border.all(width: 2.0),
-      //       borderRadius: BorderRadius.all(
-      //           Radius.circular(15.0) //                 <--- border radius here
-      //           ),
-      //     ),
-      //     child: Container(
-      //       width: double.infinity,
-      //       height: 640,
-      //       child: Column(
-      //         children: [
-      //           SizedBox(
-      //             height: 5,
-      //           ),
-      //           Scrollbar(
-      //             child: Container(
-      //               height: 600,
-      //               child: ListView.builder(
-      //                   itemCount: data.length,
-      //                   itemBuilder: ((context, index) {
-      //                     return Card(
-      //                       child: Container(
-      //                         color: Color.fromARGB(255, 76, 164, 174),
-      //                         padding: EdgeInsets.all(15),
-      //                         child: Column(
-      //                           children: [
-      //                             Row(
-      //                               mainAxisAlignment:
-      //                                   MainAxisAlignment.spaceAround,
-      //                               crossAxisAlignment:
-      //                                   CrossAxisAlignment.center,
-      //                               children: [
-      //                                 const Icon(
-      //                                   Icons.notifications_active,
-      //                                   size: 40,
-      //                                   color: Colors.white,
-      //                                 ),
-      //                                 Column(
-      //                                   children: [
-      //                                     Text(
-      //                                       ' ${data[index]["title"]}',
-      //                                       textScaleFactor: 1.5,
-      //                                       style: TextStyle(
-      //                                         fontSize: 12,
-      //                                         fontWeight: FontWeight.bold,
-      //                                         fontFamily: 'Open Sans',
-      //                                         // fontWeight: FontWeight.bold,
-      //                                         color: Colors.white,
-      //                                       ),
-      //                                     ),
-      //                                     Text(
-      //                                       ' ${data[index]["title"]}',
-      //                                       style: TextStyle(
-      //                                           fontSize: 15,
-      //                                           fontWeight: FontWeight.bold,
-      //                                           fontFamily: 'Open Sans',
-      //                                           // fontWeight: FontWeight.bold,
-      //                                           color: Colors.white70),
-      //                                     ),
-      //                                   ],
-      //                                 ),
-      //                                 Icon(
-      //                                   Icons.not_started,
-      //                                   color: Colors.white,
-      //                                 ),
-      //                               ],
-      //                             ),
-      //                             SizedBox(
-      //                               height: 10,
-      //                             ),
-      //                             Row(
-      //                               mainAxisAlignment:
-      //                                   MainAxisAlignment.spaceAround,
-      //                               children: [
-      //                                 Text(
-      //                                   '12/02/2023',
-      //                                   style: TextStyle(
-      //                                     fontSize: 12,
-      //                                     fontFamily: 'Open Sans',
-      //                                     fontWeight: FontWeight.bold,
-      //                                     color: Colors.white,
-      //                                   ),
-      //                                 ),
-      //                                 Text(
-      //                                   '2.30Pm',
-      //                                   style: TextStyle(
-      //                                     fontSize: 12,
-      //                                     fontFamily: 'Open Sans',
-      //                                     fontWeight: FontWeight.bold,
-      //                                     color: Colors.white,
-      //                                   ),
-      //                                 ),
-      //                               ],
-      //                             ),
-      //                           ],
-      //                         ),
-      //                       ),
-      //                     );
-      //                   })),
-      //             ),
-      //           )
-      //         ],
-      //       ),
-      //     ),
-      //   ),
-      // ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            // color: Colors.lightGreen[400],
+            color: Color.fromARGB(255, 255, 255, 255),
+            border: Border.all(width: 2.0),
+            borderRadius: BorderRadius.all(
+                Radius.circular(15.0) //                 <--- border radius here
+                ),
+          ),
+          child: Container(
+            width: double.infinity,
+            height: 640,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 5,
+                ),
+                Scrollbar(
+                  child: Container(
+                    height: 600,
+                    child: ListView.builder(
+                        itemCount: data.length,
+                        itemBuilder: ((context, index) {
+                          return Card(
+                            child: Container(
+                              color: Color.fromARGB(255, 76, 164, 174),
+                              padding: EdgeInsets.all(15),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      const Icon(
+                                        Icons.notifications_active,
+                                        size: 40,
+                                        color: Colors.white,
+                                      ),
+                                      Column(
+                                        children: [
+                                          Text(
+                                            ' ${data[index]["title"]}',
+                                            textScaleFactor: 1.5,
+                                            style: TextStyle(
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.bold,
+                                              fontFamily: 'Open Sans',
+                                              // fontWeight: FontWeight.bold,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                          Text(
+                                            ' ${data[index]["title"]}',
+                                            style: TextStyle(
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.bold,
+                                                fontFamily: 'Open Sans',
+                                                // fontWeight: FontWeight.bold,
+                                                color: Colors.white70),
+                                          ),
+                                        ],
+                                      ),
+                                      Icon(
+                                        Icons.not_started,
+                                        color: Colors.white,
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Text(
+                                        '12/02/2023',
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          fontFamily: 'Open Sans',
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      Text(
+                                        '2.30Pm',
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          fontFamily: 'Open Sans',
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        })),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
